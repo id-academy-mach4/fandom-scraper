e6940960340606d5d1bdce502c2304fc1e464cf1
OrchidW1nters <135547389+OrchidW1nters@users.noreply.github.com>
2023-08-01 20:42:18 +0000

    added user prompt function to run.py

--- src/run.py
+++ src/run.py
@@ -1,7 +1,8 @@
 import string
+import setup
 
 # List of words that we will remove from our string
-wordsNotToUse = ["of", "the", "a", "when", "it", "if", "are", "so", "why", "how", "do", "to", "should", "i", "you", "u", "what", "who", "is", "cant", "cannot" ]
+wordsNotToUse = ["of", "the", "a", "when", "it", "if", "are", "so", "why", "how", "do", "to", "should", "in", "i", "you", "u", "what", "who", "is", "cant", "cannot", "way", "get", "best", "worst"]
 
 # Function to parse a string and remove punctuation and words not to use.
 def findKeywords(startingString):
@@ -10,3 +11,8 @@ def findKeywords(startingString):
     i = i.replace(word + " ", "")
 
   return i
+
+def promptUser():
+  userInput = input("What would you like to know about " + setup.fandom + "?\nPlease frame your response in the form of a question, with the name of the fandom in the question.\nFor example, \"What is the best way to collect strawberries in celestegame?\"\n")
+  keywords = findKeywords(userInput)
+  print(keywords)
