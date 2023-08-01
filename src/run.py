4578367d38e9da40deefd860ee7e445c604c21e4
Annabella Proctor <nadairpro@gmail.com>
2023-08-01 13:57:25 -0700

    Input added to run.py

--- src/run.py
+++ src/run.py
@@ -1,18 +1,47 @@
 import string
 import setup
+import model
 
 # List of words that we will remove from our string
-wordsNotToUse = ["of", "the", "a", "when", "it", "if", "are", "so", "why", "how", "do", "to", "should", "in", "i", "you", "u", "what", "who", "is", "cant", "cannot", "way", "get", "best", "worst"]
+words_not_to_use = ["of", "the", "a", "when", "it", "if", "are", "so", "why", "how", "do", "to", "should", "in", "i", "you", "u", "what", "who", "is", "cant", "cannot", "way", "get", "best", "worst"]
 
 # Function to parse a string and remove punctuation and words not to use.
-def findKeywords(startingString):
-  i = startingString.lower().translate(str.maketrans('', '', string.punctuation))
-  for word in wordsNotToUse:
-    i = i.replace(word + " ", "")
+def findKeywords(starting_string):
+  global words_not_to_use
+  i = starting_string.lower().translate(str.maketrans('', '', string.punctuation))
+  for word in words_not_to_use:
+    i = i.replace(word + " ", "").replace("celeste", "celestegame").replace("hypixel", "hypixel-skyblock").replace("hypixel-skyblock skyblock", "hypixel-skyblock")
 
   return i
 
 def promptUser():
-  userInput = input("What would you like to know about " + setup.fandom + "?\nPlease frame your response in the form of a question, with the name of the fandom in the question.\nFor example, \"What is the best way to collect strawberries in celestegame?\"\n")
+  userInput = input("What would you like to know? Please frame your response in the form of a question, with the name of the fandom in the question.\nFor example, \"What is the best way to collect strawberries in Celeste?\" (You can quit with "q".)\n\n")
+  print("")
+  # In case they want to quit
+  if(userInput.lower() == "q") return None
   keywords = findKeywords(userInput)
-  print(keywords)
+  print("DEBUG_KEYWORDS", keywords)
+  return False
+
+prompting = True
+while prompting:
+  print(">> NEW QUESTION\n")
+  x = promptUser()
+  while(x is False):
+    print(">> QUESTION ERROR\n")
+    print("Please try again! We could not parse your input...\n")
+    x = promptUser()
+  else:
+    if(x is None):
+      prompting = False
+      break
+    else:
+      print(">> RESULT\n")
+      print(x)
+      x = None
+      if(answer.lower() == "y"):
+        prompting = True
+      else:
+        prompting = False
+        break
+print("\n\nTHANKS FOR ASKING OUR PROGRAM!\nCreated by Nick, Annabella, Orchid, and Tyler.\n\n")
